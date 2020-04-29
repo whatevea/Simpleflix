a2f6fc7fceb4b7d0e3ed0fe0a9970e14f3801ab1
sandip bamrel <49833723+whatevea@users.noreply.github.com>
2020-04-29 13:09:43 +0545

    added posters href

--- script.js
+++ script.js
@@ -12,3 +12,12 @@ document.addEventListener('keydown',(e)=>{
 	if(e.keyCode==13){query=$('query').value;
 		window.location=`searchresult.html?query=${query}`;}
 })
+const hrefs=['',
+	'tt0111161',
+'tt0068646',
+'tt0071562','tt0468569','tt0050083','tt1856010','tt2309295','tt2372162','tt2189461','tt3520702','tt2431438','tt2707408']
+const myArray =[...document.querySelectorAll('img')];
+myArray.forEach((item,index)=>{
+	item.onclick=()=>{location=`series_details.html?imdbcode=${hrefs[index]}`}
+})
+console.log(myArray);
